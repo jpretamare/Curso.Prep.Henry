09767f677bead977291c425ea322fd57c401799c
random <eljugador@localhost.localdomain>
2021-02-02 13:41:52 -0300

    CP commiteado por jpretamare a las 1612284112779

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1 + n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +25,39 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  i = 0; sum = 0; do (sum += numeros[i], i++); while (i < numeros.length);cb(sum);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-}
+  for (i = 0; i< array.length; i++) {
+    let element = array[i]
+    cb(element)
+  }
+  }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  //nuevo = [];
+  //for (i=0; i <array.length; i++) {let elem = array[i];cb(elem);nuevo.push(cb(elem));}return nuevo;}
+  var nuevo = array.map(function (el) {
+    return cb(el);
+  })
+  return nuevo;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  nue = []
+  for (i=0; i < array.length; i++)
+    if (array[i][0] === "a") {nue.push(array[i])}; return nue;
 }
 
 // No modificar nada debajo de esta línea
